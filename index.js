6f1a7a2289e27c0ab686243808437349a173cc23
Melissa Flynn <mflynn814@gmail.com>
2021-07-28 15:01:38 -0700

    Add validations for signup and login, add setAuthToken util function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 
@@ -14,7 +15,10 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("BlackJack"));
+// app.get("/", (req, res) => res.send("BlackJack"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
