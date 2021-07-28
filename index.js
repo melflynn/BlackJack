bddef2fec5acc5590683d40a3d7fa47b6492ff50
Melissa Flynn <mflynn814@gmail.com>
2021-07-28 15:18:12 -0700

    set up socket listeners in index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const http = require("http");
+const socketIo = require("socket.io");
+const httpServer = http.Server(app);
+const io = socketIo(httpServer);
 
 const users = require('./routes/api/users');
 
@@ -21,6 +25,22 @@ require('./config/passport')(passport);
 
 app.use("/api/users", users);
 
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  });
+
+  socket.on("join game", ({gameId, username}) => {
+    socket.join(gameId);
+    console.log(username + " joined game: " + gameId);
+  })
+
+  socket.on("leave game", ({gameId, username}) => {
+    socket.leave(gameId);
+    console.log(username + " left game: " + gameId);
+  })
+})
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+httpServer.listen(port, () => console.log(`Server is running on port ${port}`));
