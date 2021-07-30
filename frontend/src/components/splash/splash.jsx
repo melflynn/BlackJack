61e43942c5a92ee4aff6d4075e6585231ff5494f
simpleistruth <45241193+simpleistruth@users.noreply.github.com>
2021-07-30 00:43:08 -0700

    edited styling so splash page looks good even when on smaller resolutions

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,11 +1,14 @@
+import { useHistory } from "react-router-dom";
 import "../../styles/splash_page.css";
 
 const Splash = () => {
+    const hist = useHistory();
+
     return (
         <div className="splash-page-container">
             <div className="intro">Let's play BlackJack!</div>
             <div className="get-started">
-                <button className="get-started-button">get started</button>
+                <button className="get-started-button" onClick={() => hist.push('/signup')}>get started</button>
             </div>
             <div className="rules">
                 <h3 className="rules-header">Rules of BlackJack</h3>
