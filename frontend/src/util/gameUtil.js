ffc1fc192fdb7072fb29cbf418038cadf24ac372
Melissa Flynn <mflynn814@gmail.com>
2021-08-01 22:14:23 -0700

    Add game routes to util

--- frontend/src/util/gameUtil.js
+++ frontend/src/util/gameUtil.js
@@ -3,3 +3,15 @@ import axios from 'axios';
 export const createGame = () => {
   return axios.post('/api/games/');
 }
+
+export const drawCard = () => {
+  return axios.patch('/api/games/draw');
+}
+
+export const addPlayer = (userId) => {
+  return axios.patch('/api/games/newPlayer', userId);
+}
+
+export const switchTurn = () => {
+  return axios.patch('/api/games/turn');
+}
