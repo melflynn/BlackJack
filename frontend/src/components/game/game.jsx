1e80f5b4f0a3c10de43e4979543901c17044822c
Melissa Flynn <mflynn814@gmail.com>
2021-08-02 12:02:31 -0700

    Import button to game component

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { Button } from '@material-ui/core'
 import io from 'socket.io-client';
 
 const InformationWrapper = styled.div`
