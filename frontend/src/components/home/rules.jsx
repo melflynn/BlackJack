89a8654b8f61969809e51230dfaa0a6c415501cb
Michael Lau <lau.michael2@gmail.com>
2021-08-02 14:23:26 -0700

    added rules to splash and rules page

--- frontend/src/components/home/rules.jsx
+++ frontend/src/components/home/rules.jsx
@@ -11,13 +11,19 @@ class Rules extends React.Component {
       <div className="rules-page-container">
         <div className="rules-page">
           <h3 className="rules-header">Rules of BlackJack</h3>
-          <p className="rules-bullet">1. Something something something something</p>
-          <p className="rules-bullet">2. Something something something something</p>
-          <p className="rules-bullet">4. Something something something something</p>
-          <p className="rules-bullet">5. Something something something something</p>
-          <p className="rules-bullet">6. Something something something something</p>
-          <p className="rules-bullet">7. Something something something something</p>
-          <p className="rules-bullet">8. Something something something something</p>
+          <p className="rules-bullet">1. The goal of blackjack is to beat the dealer's hand without going over 21.</p>
+          <p className="rules-bullet">2. Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.</p>
+          <p className="rules-bullet">3. Each player starts with two cards, one of the dealer's cards is hidden until the end.</p>
+          <p className="rules-bullet">4. To 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn.</p>
+          <p className="rules-bullet">5. If you go over 21 you bust, and the dealer wins regardless of the dealer's hand.</p>
+          <p className="rules-bullet">6. If you are dealt 21 from the start (Ace & 10), you got a blackjack.</p>
+          <p className="rules-bullet">7. Dealer will hit until his/her cards total 17 or higher.</p>
+          <p className="rules-bullet">8. Doubling is like a hit, only the bet is doubled and you only get one more card.</p>
+          <p className="rules-bullet">9. Split can be done when you have two of the same card - the pair is split into two hands.</p>
+          <p className="rules-bullet">10. Splitting also doubles the bet, because each new hand is worth the original bet.</p>
+          <p className="rules-bullet">11. You can only double/split on the first move, or first move of a hand created by a split.</p>
+          <p className="rules-bullet">12. You cannot play on two aces after they are split.</p>
+          <p className="rules-bullet">13. You can double on a hand resulting from a split, tripling or quadrupling you bet.</p>
         </div>
       </div>
     )
